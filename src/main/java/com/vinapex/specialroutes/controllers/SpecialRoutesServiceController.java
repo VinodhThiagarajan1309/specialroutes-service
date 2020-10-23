474290aa09168125fdf5df7d7e43d5388d15fef5
Vinodh Thiagarajan <vinodh.thiagarajan@gmail.com>
2020-10-23 10:00:53 -0500

    Special routes filer db pointing to new Schema

---
+++ src/main/java/com/vinapex/specialroutes/controllers/SpecialRoutesServiceController.java
@@ -0,0 +1,24 @@
+package com.vinapex.specialroutes.controllers;
+
+import com.vinapex.specialroutes.model.AbTestingRoute;
+import com.vinapex.specialroutes.service.AbTestingRouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping(value="v1/route/")
+public class SpecialRoutesServiceController {
+
+    @Autowired
+    AbTestingRouteService routeService;
+
+    @RequestMapping(value="abtesting/{serviceName}",method = RequestMethod.GET)
+    public AbTestingRoute abstestings(@PathVariable("serviceName") String serviceName) {
+
+        return routeService.getRoute( serviceName);
+    }
+
+}
